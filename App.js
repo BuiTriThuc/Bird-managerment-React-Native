463e9f5cea85f2f351acefa28296a599186ae550
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-06-27 23:23:22 +0700

    Thuc update Bird

--- App.js
+++ App.js
@@ -3,10 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignIn from "./src/components/pages/Account/SignIn";
-import Product from "./src/components/pages/Shop/Product";
-import BirdFood from "./src/components/pages/Shop/BirdFood";
-import ProductDetail from "./src/components/pages/Shop/ProductDetail/ProductDetail";
-import BirdFoodDetail from "./src/components/pages/Shop/BirdFoodDetail/BirdFoodDetail";
+
+import BirdScreen from "./src/components/pages/Shop/Bird/BirdScreen";
+import BirdDetail from "./src/components/pages/Shop/Bird/BirdDetail";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -14,10 +13,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         {/* <Stack.Screen name="SignIn" component={SignIn} /> */}
-        <Stack.Screen name="Product" component={Product} />
-        <Stack.Screen name="ProductDetail" component={ProductDetail} />
-        <Stack.Screen name="BirdFood" component={BirdFood} />
-        <Stack.Screen name="BirdFoodDetail" component={BirdFoodDetail} />
+        <Stack.Screen name="Bird" component={BirdScreen} />
+        <Stack.Screen name="BirdDetail" component={BirdDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
